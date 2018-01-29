d2bb8f6076144d6c4bb14d3b4a6f1951dad82ad5
Eric Pricop <dimitripricop@yahoo.com>
2018-01-28 19:02:40 -0800

    modefied time.js into a function

--- time.js
+++ time.js
@@ -1,24 +1,25 @@
-var HOUR = 6; 
-var MINUTE = 30; 
-var PERIOD = 'PM'; 
-if (MINUTE > 30) {
-	if (MINUTE == 45) {
-		console.log("It's quarter to", HOUR + 1)
+function Time(HOUR, MINUTE, PERIOD) {
+
+	if (MINUTE > 30) {
+		if (MINUTE == 45) {
+			console.log("It's quarter to", HOUR + 1, PERIOD)
+		}
+		else {
+		console.log("It's almost", HOUR, PERIOD);
+		}
+	}
+	else if(MINUTE < 30){
+		if (MINUTE == 15) {
+			console.log("It's quarter after", HOUR, PERIOD)
+		}
+		else {
+			console.log("It's just after", HOUR, PERIOD);
+		}
 	}
 	else {
-	console.log("It's almost", HOUR, PERIOD);
+		console.log("It's half past", HOUR, PERIOD)
 	}
-}
-else if(MINUTE < 30){
-	if (MINUTE == 15) {
-		console.log("It's quarter after", HOUR)
-	}
-	else {
-		console.log("It's just after", HOUR, PERIOD);
-	}
-}
-else {
-	console.log("It's half past", HOUR)
 }
 
+Time(6, 05, 'PM');
 
